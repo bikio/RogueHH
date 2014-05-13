a61fb2e1a3779a2207156c60c5d51782a9abe2ec
ageobab <agbabiki@gmail.com>
2014-05-13 18:06:07 -0400

    Bootstrap and makings of a navbar. Images added.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HOOK::Application.config.secret_key_base = '49860f261b6fd2be0f8d8ec4968a623a772c7b484d91efdfeea391ff7902ae1a1079ad00b54eb4ba7f6f8f53eb4e44826e4caa060712f707dc9079e332af50b5'
