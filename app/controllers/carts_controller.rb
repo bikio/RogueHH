48c92c6a8d9de585f6bd927de30ec7e4cc18a2a2
ageobab <agbabiki@gmail.com>
2014-08-05 02:15:22 -0400

    minibugs fixed, working add to cart button leads to nightmare CSS

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,17 @@
-class CartsController > ApplicationController
+class CartsController < ApplicationController
 # GET /carts/1
 # GET /carts/1.xml 
-def show
-  begin
-    @cart = Cart.find(params[:id]) 
-  rescue ActiveRecord::RecordNotFound
-    logger.error "Attempt to access invalid cart #{params[:id]}"
-    redirect_to store_url, :notice => 'Invalid cart'
-  else
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @cart }
-    end 
+  def show
+    begin
+      @cart = Cart.find(params[:id]) 
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, :notice => 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @cart }
+      end 
+    end
   end
 end
