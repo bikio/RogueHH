4f9e5373687634ecdd184b61cf66e7449e68602e
ageobab <agbabiki@gmail.com>
2014-08-07 18:03:07 -0400

    cart math

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,10 @@ class Cart < ActiveRecord::Base
   def add_product(product_id)
     line_items.find_or_initialize_by(product_id: product_id).increment(:quantity)
   end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+
+
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,10 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :cart
 
+  def total_price
+    product.price * quantity
+  end
+
   def copy_product_attributes(product)
     self.name = product.name
     self.price = product.price
