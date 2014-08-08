17a938044f4730f3beeb76c13517916230b4961c
ageobab <agbabiki@gmail.com>
2014-08-08 16:37:33 -0400

    no more bogus cart view whenever add to cart is clicked, instead, redirect to /#products

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -15,7 +15,7 @@ def create
 
   respond_to do |format| 
     if @line_item.save
-      format.html { redirect_to(@line_item.cart) }
+      format.html { redirect_to "/#products" }
       format.xml  { render :xml => @line_item,
         :status => :created, :location => @line_item }
     else
