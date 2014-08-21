ca6830e4240034d5fe5d2c9dbb74a30e696d28f5
ageobab <agbabiki@gmail.com>
2014-08-21 17:26:39 -0400

    kill all migrations affiliated w product

--- db/migrate/20140804201919_add_product_id_to_line_items.rb
+++
@@ -1,5 +0,0 @@
-class AddProductIdToLineItems < ActiveRecord::Migration
-  def change
-    add_column :line_items, :product_id, :integer
-  end
-end
--- db/migrate/20140804214101_add_product_id_to_carts.rb
+++
@@ -1,5 +0,0 @@
-class AddProductIdToCarts < ActiveRecord::Migration
-  def change
-    add_column :carts, :product_id, :integer
-  end
-end
