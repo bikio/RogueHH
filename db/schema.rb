a5d23586561dc36e83e1ec057ecabafae0048493
ageobab <agbabiki@gmail.com>
2014-08-23 17:06:15 -0400

    migrations- add product_id to carts and line_items for working add to cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821213643) do
+ActiveRecord::Schema.define(version: 20140823210403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140821213643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "cart_id"
+    t.integer  "product_id"
   end
 
   create_table "colors", force: true do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 20140821213643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "cart_id"
+    t.integer  "product_id"
   end
 
   create_table "orders", force: true do |t|
