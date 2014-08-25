8685315d95f13d4256b31b7ffaac6a3a04c6d645
ageobab <agbabiki@gmail.com>
2014-08-25 02:32:11 -0400

    javascript scroll now lands correctly on div

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,6 +1,7 @@
 $(document).on("click", "a", function() {
   $('html, body').animate({
-    scrollTop: $(this.hash).position().top
+    scrollTop: $(this.hash).position().top-100
   }, 1200);
   return false;
 });
+
