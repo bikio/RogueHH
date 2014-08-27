1791290949f47e0f2884f1efb1b2cb3eea373ff2
ageobab <agbabiki@gmail.com>
2014-08-27 16:22:02 -0400

    products section now resizes properly with window, javascript touch for underscroll

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,6 +1,6 @@
 $(document).on("click", "a", function() {
   $('html, body').animate({
-    scrollTop: $(this.hash).position().top-100
+    scrollTop: $(this.hash).position().top-90
   }, 1200);
   return false;
 });
